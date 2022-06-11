0cb42bfcb09663b4324b000ba96d88cdfd024a47
AmirHossein-Falakian <amirfalakian4@gmail.com>
2022-06-11 16:39:39 +0430

    fix use effect warning

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -11,16 +11,16 @@ const Recipe = () => {
   const [details, setDetails] = useState({});
   const [active, setActive] = useState("instuctions");
   let params = useParams();
-  const fetchDetails = async () => {
-    const data = await fetch(
-      `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
-    );
-    const detailData = await data.json();
-    setDetails(detailData);
-    console.log(detailData);
-  };
 
   useEffect(() => {
+    const fetchDetails = async () => {
+      const data = await fetch(
+        `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
+      );
+      const detailData = await data.json();
+      setDetails(detailData);
+      console.log(detailData);
+    };
     fetchDetails();
   }, [params.name]);
 
